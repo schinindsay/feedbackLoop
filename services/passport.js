5e21aff0195f91f9626ec228dec7636f81f54ce6
Lindsay Schinasi <lindsay.schinasi@gmail.com>
2018-09-22 12:57:44 -0400

    deploying to heroku

--- services/passport.js
+++ services/passport.js
@@ -1,7 +1,7 @@
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20').Strategy
 const mongoose = require('mongoose')
-const keys = require('../config/dev')
+const keys = require('../config/keys')
 
 const User = mongoose.model('users')
 
@@ -20,19 +20,21 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
+      callbackURL: '/auth/google/callback',
+      proxy: true
     },
     (accessToken, refreshToken, profile, done) => {
       User.findOne({ googleId: profile.id }).then(existingUser => {
         if (existingUser) {
           // we already have a record with the given profile ID
-          done(null, existingUser)
+          done(null, existingUser);
         } else {
           // we don't have a user record with this ID, make a new record!
           new User({ googleId: profile.id })
             .save()
-            .then(user => done(null, user))
+            .then(user => done(null, user));
         }
-      })
-    })
+      });
+    }
   )
+);
