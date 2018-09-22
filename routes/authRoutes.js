2045925155f0e0b6dbd90244f751217bc12bd24a
Lindsay Schinasi <lindsay.schinasi@gmail.com>
2018-09-22 12:28:17 -0400

    initial commit

---
+++ routes/authRoutes.js
@@ -0,0 +1,28 @@
+const passport = require('passport')
+
+//create a get route for /auth/google (could have called it anything)
+module.exports = app => {
+  app.get(
+    '/auth/google',
+    passport.authenticate('google', {
+      scope: ['profile', 'email']
+    })
+  )
+
+  app.get('/auth/google/callback', passport.authenticate('google'), (req, res, err) => {
+
+    res.send('logged in!')
+
+  })
+
+  app.get('/api/current_user', (req, res) => {
+    res.send(req.user)
+  })
+
+  app.get('/api/logout', (req, res) => {
+    req.logout()
+    res.send(req.user)
+  })
+}
+
+
